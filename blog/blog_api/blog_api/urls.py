f0670742ce90b405b5caf9277b458e8540ae28b7
marcuss <marcuss@hesonline.com>
2016-07-29 13:23:38 -0400

    write test view

--- blog/blog_api/blog_api/urls.py
+++ blog/blog_api/blog_api/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 
-import main
+from main import urls as main_urls
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^api/', main.urls)
+    url(r'^api/', main_urls)
 ]
