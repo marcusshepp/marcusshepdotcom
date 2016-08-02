d1125b975b7da76ef4e0947a80cf50879aa0e0e6
Marcus Shepherd <marcusshepdotcom@gmail.com>
2016-08-02 16:25:35 -0400

    lol

--- blog/blog_api/main/views.py
+++ blog/blog_api/main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 
 # Create your views here.
 def foo(request, *a, **kw):
